c40babbc1afbf0185e7766e71a5b50d8667e18ab
createiq-test <test@createiq.com>
2021-08-12 10:12:29 +0530

    n/a

--- E-commerce-Admin/src/main/java/com/createiq/pw/controller/BookController.java
+++ E-commerce-Admin/src/main/java/com/createiq/pw/controller/BookController.java
@@ -1,6 +1,11 @@
 package com.createiq.pw.controller;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,15 +13,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.createiq.pw.domin.Book;
 import com.createiq.pw.domin.ResponseBean;
 import com.createiq.pw.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
+	
+	private static final String UPLOAD_FOLDER = "D:\\Balaji-test\\";
+	
 	@Autowired
 	private BookService bookService;
 
@@ -36,6 +50,41 @@ public class BookController {
 		return responseBean;
 	}
 
+	
+	@PostMapping(value="/saveWithImg", consumes =MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseBean saveWithImg(@RequestPart("book") String bookString , @RequestPart("file") MultipartFile file) {
+		
+		Book book = new Book();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			book = mapper.readValue(bookString, Book.class);
+		} catch (JsonMappingException e1) {
+			e1.printStackTrace();
+		} catch (JsonProcessingException e1) {
+			e1.printStackTrace();
+		}
+		
+		book  = bookService.saveOrUpdate(book);
+
+		try {
+			// read and write the file to the selected location-
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(UPLOAD_FOLDER +book.getId()+".png");
+			Files.write(path, bytes);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		ResponseBean responseBean = new ResponseBean();
+		responseBean.setStatus(201);
+		responseBean.setResult(book);
+		return responseBean;
+	}
+
+	
 	@PutMapping
 	public ResponseBean update(@RequestBody Book book) {
 		ResponseBean responseBean = new ResponseBean();
--- E-commerce-Admin/src/main/java/com/createiq/pw/domin/Book.java
+++ E-commerce-Admin/src/main/java/com/createiq/pw/domin/Book.java
@@ -5,11 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
 
 @Entity
+@Table(name= "book_tab")
 public class Book {
 
 	@Id
